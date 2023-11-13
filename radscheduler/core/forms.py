d31b0542c7966c63279f74947fff7686cf951009
tubo <shitubo@gmail.com>
2023-11-13 08:55:26 +0000

    better form error message

--- radscheduler/core/forms.py
+++ radscheduler/core/forms.py
@@ -1,13 +1,14 @@
 from typing import Any
 
 from django import forms
+from django.forms.formsets import formset_factory
 
-from radscheduler.core.models import Leave
+from radscheduler.core.models import Leave, Shift
 
 
 class DateRangeForm(forms.Form):
-    start = forms.DateField(required=False)
-    end = forms.DateField(required=False)
+    start = forms.DateField(required=True)
+    end = forms.DateField(required=True)
 
     def clean(self) -> dict[str, Any]:
         cleaned_data = super().clean()
@@ -15,7 +16,7 @@ class DateRangeForm(forms.Form):
         end = cleaned_data.get("end")
 
         if start and end and start > end:
-            raise forms.ValidationError("Start date must be before end date")
+            self.add_error("start", "Start date must be before end date")
 
 
 class LeaveForm(forms.ModelForm):
