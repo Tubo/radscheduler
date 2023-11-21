00f153c72502f1715f105a925f27e70837cbbb44
tubo <shitubo@gmail.com>
2023-11-21 07:52:07 +0000

    minor refactor of leave page

--- radscheduler/core/views/leaves.py
+++ radscheduler/core/views/leaves.py
@@ -2,27 +2,46 @@ from datetime import timedelta
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404, redirect, render
 
 from radscheduler.core.forms import LeaveForm
 from radscheduler.core.models import Leave
+from radscheduler.roster import canterbury_holidays
 
 
 @login_required
 def leave_page(request):
+    try:
+        registrar = request.user.registrar
+    except ObjectDoesNotExist:
+        messages.error(request, "You are not a registrar.")
+        return redirect("home")
+
     if request.method == "POST":
         form = LeaveForm(request.POST)
         if form.is_valid():
             leave = form.save()
             next_date = leave.date + timedelta(days=1) if leave.date.weekday() < 4 else leave.date + timedelta(days=3)
-            form = LeaveForm(initial={"date": next_date, "type": leave.type, "registrar": request.user.registrar})
-            messages.info(request, f"Leave added for {leave.date.strftime('%d/%m/%Y')}")
+            form = LeaveForm(
+                initial={
+                    "date": next_date,
+                    "type": leave.type,
+                    "portion": leave.portion,
+                    "registrar": registrar,
+                }
+            )
+            msg = f"Leave added for {leave.date.strftime('%d/%m/%Y')}"
+            if leave.date in canterbury_holidays:
+                msg += f" ({canterbury_holidays[leave.date]})"
+            messages.info(request, msg)
         else:
             for error in form.non_field_errors():
                 messages.error(request, error)
+            form.initial = request.POST
     else:
-        form = LeaveForm(initial={"registrar": request.user.registrar})
-    rows = Leave.objects.filter(registrar=request.user.registrar).order_by("-date")
+        form = LeaveForm(initial={"registrar": registrar})
+    rows = Leave.objects.filter(registrar=registrar).order_by("-date")
     return render(request, "leaves/page.html", {"form": form, "rows": rows})
 
 
