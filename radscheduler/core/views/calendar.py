3ceb302b29c54c153d51cbbe2bf780236be5867e
tubo <shitubo@gmail.com>
2024-02-23 08:58:07 +0000

    Refactor leave filtering in get_calendar() function to exclude rejected

--- radscheduler/core/views/calendar.py
+++ radscheduler/core/views/calendar.py
@@ -22,8 +22,11 @@ def get_calendar(request):
             shifts = Shift.objects.filter(date__gte=start, date__lte=end, registrar__isnull=False).select_related(
                 "registrar", "registrar__user"
             )
-            leaves = Leave.objects.filter(date__gte=start, date__lte=end, cancelled=False).select_related(
-                "registrar", "registrar__user"
+            leaves = (
+                Leave.objects.filter(date__gte=start, date__lte=end, cancelled=False)
+                .exclude(reg_approved=False)
+                .exclude(dot_approved=False)
+                .select_related("registrar", "registrar__user")
             )
             events = shifts_to_events(shifts) + leaves_to_events(leaves) + holidays_to_events(start.year)
             return JsonResponse(events, safe=False)
