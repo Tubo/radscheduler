42065d069d50e956683a60fb5091ce3c8ec20c26
tubo <shitubo@gmail.com>
2024-06-19 23:39:30 +0000

    change to list view on smaller devices

--- radscheduler/static/js/calendar.js
+++ radscheduler/static/js/calendar.js
@@ -1,13 +1,14 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 import nzLocale from '@fullcalendar/core/locales/en-nz';
 
 const calendarEl = document.getElementById('calendar');
 
 let calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin, bootstrap5Plugin],
-    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin, listPlugin, bootstrap5Plugin],
+    initialView: window.innerWidth > 768 ? 'dayGridMonth' : 'listWeek',
     themeSystem: 'bootstrap5',
     events: "/api/calendar/events/",
     locale: nzLocale,
