46d2b601774949c3e1090bd0282f46b75ba63a5d
tubo <shitubo@gmail.com>
2024-06-20 01:14:15 +0000

    add responsiveness to calendar

--- radscheduler/static/js/calendar.js
+++ radscheduler/static/js/calendar.js
@@ -9,6 +9,14 @@ const calendarEl = document.getElementById('calendar');
 let calendar = new Calendar(calendarEl, {
     plugins: [dayGridPlugin, listPlugin, bootstrap5Plugin],
     initialView: window.innerWidth > 768 ? 'dayGridMonth' : 'listWeek',
+    windowResize: function (view) {
+        // If the window is resized to a width greater than 768px, change the view to dayGridMonth
+        if (window.innerWidth > 768) {
+            calendar.changeView('dayGridMonth');
+        } else {
+            calendar.changeView('listWeek');
+        }
+    },
     themeSystem: 'bootstrap5',
     events: "/api/calendar/events/",
     locale: nzLocale,
