a7194305365ec85d27b7345120b317724895d275
Tubo Shi <tubo@tubo.nz>
2024-12-31 09:20:53 +0000

    use django-ninja for domain mapper

---
+++ radscheduler/core/domain_mapper.py
@@ -0,0 +1,100 @@
+import dataclasses
+from datetime import date
+from typing import List, Optional
+
+from ninja import Field, ModelSchema, Schema
+
+import radscheduler.core.models as orm
+import radscheduler.roster.models as domain
+
+# This module provides mapping from Django ORM to the domain models
+
+
+class RegistrarDomainSchema(ModelSchema):
+    id: int = Field(..., alias="pk")
+    username: str = Field(..., alias="user.username")
+
+    class Meta:
+        model = orm.Registrar
+        fields = ["senior", "start", "finish"]
+
+
+class LeaveDomainSchema(ModelSchema):
+    type: domain.LeaveType
+    registrar: RegistrarDomainSchema
+
+    class Meta:
+        model = orm.Leave
+        fields = ["date", "type", "registrar", "no_abutting_weekend"]
+
+
+class StatusDomainSchema(ModelSchema):
+    registrar: RegistrarDomainSchema
+    type: domain.StatusType
+    weekdays: List[domain.Weekday]
+    shift_types: List[domain.ShiftType]
+
+    class Meta:
+        model = orm.Status
+        fields = ["registrar", "type", "weekdays", "shift_types", "start", "end"]
+
+
+class ShiftDomainSchema(ModelSchema):
+    type: domain.ShiftType
+    registrar: RegistrarDomainSchema
+
+    class Meta:
+        model = orm.Shift
+        fields = ["date", "type", "registrar", "stat_day", "extra_duty", "fatigue_override", "series"]
+
+
+class ShiftOrmSchema(ModelSchema):
+    type: domain.ShiftType
+    registrar_id: int = Field(None, alias="registrar.id")
+
+    class Meta:
+        model = orm.Shift
+        fields = ["date", "type", "stat_day", "extra_duty", "fatigue_override", "series"]
+        optional_fields = ["registrar"]
+
+
+def shift_to_db(shift: domain.Shift):
+    return orm.Shift(
+        registrar_id=shift.registrar.id,
+        date=shift.date,
+        type=shift.type.value,
+        stat_day=shift.stat_day,
+        extra_duty=shift.extra_duty,
+        fatigue_override=shift.fatigue_override,
+        series=shift.series,
+    )
+
+
+def shift_to_dict(shift):
+    return {
+        "date": str(shift.date),
+        "type": domain.ShiftType(shift.type).label,
+        "registrar": shift.registrar.pk if shift.registrar else None,
+        "username": shift.registrar.user.username if shift.registrar else None,
+        "extra_duty": shift.extra_duty,
+    }
+
+
+def leave_to_dict(leave):
+    return {
+        "date": str(leave.date),
+        "type": domain.LeaveType(leave.type).label,
+        "registrar": leave.registrar.pk if leave.registrar else None,
+        "portion": leave.portion,
+    }
+
+
+def status_to_dict(status):
+    return {
+        "start": str(status.start),
+        "end": str(status.end),
+        "type": domain.StatusType(status.type).label,
+        "registrar": status.registrar.pk,
+        "weekdays": status.weekdays,
+        "shift_types": status.shift_types,
+    }
--- radscheduler/core/tests/test_mapper.py
+++ radscheduler/core/tests/test_mapper.py
@@ -1,63 +1,91 @@
 from datetime import date
 
-import radscheduler.core.mapper as mapper
-import radscheduler.core.models as db
-import radscheduler.roster.models as py
+from dacite import from_dict
+
+import radscheduler.core.domain_mapper as domain_mapper
+import radscheduler.core.models as orm
+import radscheduler.roster.models as domain
 
 
 def test_registrar_from_db(juniors_db):
-    r_db = juniors_db[0]
-    r_py = mapper.registrar_from_db(r_db)
-    r = py.Registrar(username=r_db.user.username, senior=False, start=r_db.start)
-    assert r_py == r
+    reg_in_db = juniors_db[0]
+    mapped = domain_mapper.RegistrarDomainSchema.from_orm(reg_in_db).dict()
+    reg_in_domain = domain.Registrar(
+        username=reg_in_db.user.username, senior=False, start=reg_in_db.start, id=reg_in_db.pk
+    )
+    assert from_dict(domain.Registrar, mapped) == reg_in_domain
 
 
 def test_leave_from_db(juniors_db):
-    l_db = db.Leave.objects.create(
+    leave_in_db = orm.Leave.objects.create(
         date=date(2021, 1, 1),
-        type=py.LeaveType.ANNUAL,
+        type=domain.LeaveType.ANNUAL,
         registrar=juniors_db[0],
     )
-    l_py = mapper.leave_from_db(l_db)
-    l = py.Leave(
-        date=l_db.date,
-        type=py.LeaveType(l_db.type),
-        registrar=py.Registrar(username=l_db.registrar.user.username, senior=False, start=l_db.registrar.start),
+    mapped = domain_mapper.LeaveDomainSchema.from_orm(leave_in_db).dict()
+    leave_in_domain = domain.Leave(
+        date=leave_in_db.date,
+        type=domain.LeaveType(leave_in_db.type),
+        registrar=domain.Registrar(
+            username=leave_in_db.registrar.user.username,
+            senior=False,
+            start=leave_in_db.registrar.start,
+            id=juniors_db[0].pk,
+        ),
     )
-    assert l_py == l
+    assert from_dict(domain.Leave, mapped) == leave_in_domain
 
 
 def test_status_from_db(juniors_db):
-    s_db = db.Status.objects.create(
+    status_in_db = orm.Status.objects.create(
         start=date(2021, 1, 1),
         end=date(2021, 1, 2),
-        type=py.StatusType.BUDDY,
+        type=domain.StatusType.BUDDY,
         registrar=juniors_db[0],
         weekdays=[0, 1, 2, 3, 4, 5, 6],
-        shift_types=[py.ShiftType.LONG],
+        shift_types=[domain.ShiftType.LONG],
     )
-    s_py = mapper.status_from_db(s_db)
-    s = py.Status(
-        start=s_db.start,
-        end=s_db.end,
-        type=py.StatusType(py.StatusType.BUDDY),
-        registrar=py.Registrar(username=juniors_db[0].user.username, senior=False, start=juniors_db[0].start),
-        weekdays=[py.Weekday(x) for x in s_db.weekdays],
-        shift_types=[py.ShiftType.LONG],
+    mapped = domain_mapper.StatusDomainSchema.from_orm(status_in_db).dict()
+    registrar_in_domain = domain.Registrar(
+        username=juniors_db[0].user.username, senior=False, start=juniors_db[0].start, id=juniors_db[0].pk
     )
-    assert s_py == s
+    status_in_domain = domain.Status(
+        start=status_in_db.start,
+        end=status_in_db.end,
+        type=domain.StatusType(domain.StatusType.BUDDY),
+        registrar=registrar_in_domain,
+        weekdays=[domain.Weekday(x) for x in status_in_db.weekdays],
+        shift_types=[domain.ShiftType.LONG],
+    )
+    assert from_dict(domain.Status, mapped) == status_in_domain
 
 
 def test_shift_from_db(juniors_db):
-    s_db = db.Shift.objects.create(
+    shift_in_db = orm.Shift.objects.create(
         date=date(2021, 1, 1),
-        type=py.ShiftType.LONG,
+        type=domain.ShiftType.LONG,
         registrar=juniors_db[0],
     )
-    s_py = mapper.shift_from_db(s_db)
-    s = py.Shift(
-        date=date(2021, 1, 1),
-        type=py.ShiftType.LONG,
-        registrar=py.Registrar(username=juniors_db[0].user.username, senior=False, start=juniors_db[0].start),
+    mapped = domain_mapper.ShiftDomainSchema.from_orm(shift_in_db).dict()
+    registrar_in_domain = domain.Registrar(
+        username=juniors_db[0].user.username, senior=False, start=juniors_db[0].start, id=juniors_db[0].pk
     )
-    assert s.same_shift(s_py)
+    shift_in_domain = domain.Shift(
+        date=date(2021, 1, 1),
+        type=domain.ShiftType.LONG,
+        registrar=registrar_in_domain,
+    )
+    assert from_dict(domain.Shift, mapped) == shift_in_domain
+
+
+def test_shift_to_db(juniors_db):
+    registrar_in_domain = domain.Registrar(
+        username=juniors_db[0].user.username, senior=False, start=juniors_db[0].start, id=juniors_db[0].pk
+    )
+    shift_in_domain = domain.Shift(
+        date=date(2021, 1, 1),
+        type=domain.ShiftType.LONG,
+        registrar=registrar_in_domain,
+    )
+    shift_in_db = domain_mapper.shift_to_db(shift_in_domain)
+    assert shift_in_db.registrar_id == juniors_db[0].pk
